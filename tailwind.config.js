ad1fd142c0c23988a4fa7776481ff22d0ab3e12a
edm.nd <edmonddevera000@gmail.com>
2023-07-18 04:52:24 +0800

    folder change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,css}',
-            './dist/**/*.css'],
+  content: ["./src/**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
