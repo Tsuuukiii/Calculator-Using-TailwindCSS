d4d31ff923583499dddab81b2daa916b5df067cb
edm.nd <edmonddevera000@gmail.com>
2023-07-18 04:58:45 +0800

    path changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".*.{html,js,css}",
-  "./dist/**/*.{css}",
+  content: ["./**/*.{html,js,css}",
+  "./dist/**/*.css",
   "./images/**/*.{png,jpg,jpeg}"],
   theme: {
     extend: {},
